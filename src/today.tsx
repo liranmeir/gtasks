5e9ef1e2ea7d7c2e3eda69d349f1f55df0d1544b
liranmeir <liranmeir@gmail.com>
2022-10-21 15:55:04 +0300

    empty state

--- src/today.tsx
+++ src/today.tsx
@@ -1,15 +1,18 @@
 import { List } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
+import { google } from "googleapis";
 
+// const tasks = google.tasks("v1");
+
+//TODO: stoppint point move above to API LEVEL
 interface TaskListProps {
   isLoading: boolean;
 }
+
 function Today() {
   return (
-    <List searchBarPlaceholder={"search in gtasks"} isLoading={true}>
-      tasks?.length === 0 ? (
-      <List.EmptyView title="Congratulations!" description="No tasks left for today." icon="🎉" />) : ({" "}
-      {/* <List.Section title={section.name} subtitle={subtitle} key={index}></List.Section>); */}
+    <List>
+      <List.EmptyView title="Congratulations!" description="No tasks left for today." icon="🎉" />
     </List>
   );
 }
